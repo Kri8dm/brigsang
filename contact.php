46e3abfaeba55a25bb5bd6eefcf1467efc3ae20d
kri8dm <ganesh.j@kri8.dm.co.in>
2024-03-20 11:47:44 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,341 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Contact</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="./images_Gan/Bridgesang_logo.jpeg" type="image/x-icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light shadow sticky-top p-0">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <a href="./index.html"><img src="./images_Gan/Bridgesang_logo.jpeg" alt=""
+                    style="height: 60px; width: 250px;"></a>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav ms-auto p-4 p-lg-0">
+                <a href="index.html" class="nav-item nav-link active">Home</a>
+
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">About</a>
+                    <div class="dropdown-menu fade-down m-0">
+                        <a href="about.html" class="dropdown-item">About Us</a>
+                        <a href="./missionvission.html" class="dropdown-item">Our Mission and Vision</a>
+                    </div>
+                </div>
+                <a href="courses.html" class="nav-item nav-link">Courses</a>
+                <a href="./team.html" class="nav-item nav-link">Our Team</a>
+                <a href="contact.html" class="nav-item nav-link">Contact</a>
+                <a href="./gallery.html" class="nav-item nav-link">Gallery</a>
+            </div>
+            <!-- <a href="./Joinnow.html" class="btn btn-primary py-4 px-lg-5 d-none d-lg-block">Join Now<i
+                    class="fa fa-arrow-right ms-3"></i></a> -->
+        </div>
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    <div class="container-fluid bg-primary py-5 mb-5 page-header">
+        <div class="container py-5">
+            <div class="row justify-content-center">
+                <div class="col-lg-10 text-center">
+                    <h1 class="display-3 text-white animated slideInDown">Contact</h1>
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb justify-content-center">
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Home</a></li>
+                            <li class="breadcrumb-item"><a class="text-white" href="#">Pages</a></li>
+                            <li class="breadcrumb-item text-white active" aria-current="page">Contact</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Header End -->
+
+
+    <!-- Contact Start -->
+    <div class="container-xxl py-5">
+        <div class="container">
+            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
+                <h6 class="section-title bg-white text-center text-primary px-3">Contact Us</h6>
+                <h1 class="mb-5">Contact For Any Query</h1>
+            </div>
+            <div class="row g-4">
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <h5>Get In Touch</h5>
+                    <p class="mb-4">"Connect with us today, where ideas flourish and dreams take flight. Let's create something remarkable together."</p>
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                            <i class="fa fa-map-marker-alt text-white"></i>
+                        </div>
+                        <div class="ms-3">
+                            <h5 class="text-primary">Office</h5>
+                            <p class="mb-0">BridgeSang Trainings & Consultancy Pvt Ltd,
+                                5th Floor, Atharva Feryez Plaza
+                                Opposite Shankar Maharaja Math, Pune Satara Road, Dhankawadi,
+                                Katraj, Pune- 411043</p>
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center mb-3">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                            <i class="fa fa-phone-alt text-white"></i>
+                        </div>
+                        <div class="ms-3">
+                            <h5 class="text-primary">Mobile</h5>
+                            <p class="mb-2"><a href="tel:8149042119">8149042119</a></p>
+                    <p class="mb-2"><a href="tel:7262044661">7262044661</a></p>
+                    <!-- <p class="mb-2"><i class="fa fa-envelope me-3"></i><a
+                            href="mailto:info@bridgesang.com">info@bridgesang.com</a></p> -->
+                        </div>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <div class="d-flex align-items-center justify-content-center flex-shrink-0 bg-primary" style="width: 50px; height: 50px;">
+                            <i class="fa fa-envelope-open text-white"></i>
+                        </div>
+                        <div class="ms-3">
+                            <h5 class="text-primary">Email</h5>
+                            <p class="mb-2"><a
+                                href="mailto:info@bridgesang.com">info@bridgesang.com</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1338.9413133964367!2d73.85603461566072!3d18.469700758268463!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTjCsDI4JzExLjciTiA3M8KwNTEnMjQuNiJF!5e0!3m2!1sen!2sin!4v1708512441236!5m2!1sen!2sin" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="col-lg-4 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
+                    <?php 
+                    //Acception session value 
+                    if(isset($_SESSION['message'])){ ?>
+                        <div class="alert alert-success" role="alert">
+                          <?php echo $_SESSION['message']; ?>
+                        </div>
+                    <?php }
+                    unset($_SESSION['message']);
+                    ?>
+                    <form action="" method="post"> 
+                        <div class="row g-3">
+                            <div class="col-md-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+                                    <label for="name">Your Name</label>
+                                </div>
+                            </div>
+                            <div class="col-md-12">
+                                <div class="form-floating">
+                                    <input type="email" class="form-control" id="email" name="email" placeholder="Your Email">
+                                    <label for="email">Your Email</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject">
+                                    <label for="subject">Subject</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-floating">
+                                    <textarea class="form-control" placeholder="Leave a message here" name="message" id="message" style="height: 150px"></textarea>
+                                    <label for="message">Message</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" type="submit" name="submit">Send Message</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
+
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-light footer pt-5 mt-5 wow fadeIn" data-wow-delay="0.1s">
+        <div class="container py-5">
+            <div class="row g-5">
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Quick Link</h4>
+                    <a class="btn btn-link" href="./about.html">About Us</a>
+                    <a class="btn btn-link" href="./contact.html">Contact Us</a>
+                    <a class="btn btn-link" href="#">Privacy Policy</a>
+                    <a class="btn btn-link" href="#">Terms & Condition</a>
+                    <a class="btn btn-link" href="#">FAQs & Help</a>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <h4 class="text-white mb-6">Contact</h4>
+                    <p class="mb-2">
+                        <i class="fa fa-map-marker-alt me-3"></i>
+                        BridgeSang Trainings & Consultancy Pvt Ltd,
+                        5th Floor, Atharva Feryez Plaza
+                        Opposite Shankar Maharaja Math, Pune Satara Road, Dhankawadi,
+                        Katraj, Pune- 411043
+                        </a>
+
+                    </p>
+
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:8149042119">8149042119</a></p>
+                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i><a href="tel:7262044661">7262044661</a></p>
+                    <p class="mb-2"><i class="fa fa-envelope me-3"></i><a
+                            href="mailto:info@bridgesang.com">info@bridgesang.com</a></p>
+
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href="https://www.facebook.com/bridgesang"><i
+                                class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                        <a class="btn btn-outline-light btn-social"
+                            href="https://www.instagram.com/bridge_sang?utm_source=qr&igsh=MTZ6c3JvajI3ZTFhag=="><i
+                                class="fab fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h4 class="text-white mb-3">Gallery</h4>
+                    <div class="row g-2 pt-2">
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (1).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (2).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (14).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (13).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (17).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <div class="col-4">
+                            <img class="img-fluid bg-light p-1" src="./images_Gan/Gallery (3).jpeg" alt=""
+                                loading="lazy">
+                        </div>
+                        <a class="btn btn-primary" href="./gallery.html" id="all_buttons"
+                            style="height: 35px; width: 100px;">More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End -->
+    <!-- whats app button start  -->
+    <a href="https://wa.me/+918149042119" target="_blank" id="whatsapp-button"
+        style="position: fixed; bottom: 20px; right: 20px; width: 200px; background-color: #4bcc5a; border-radius: 30px; color: white; text-decoration: none; padding: 10px 20px; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5); z-index: 1000; transition: transform 0.3s;"
+        onmouseover="this.style.transform='scale(0.9)'" onmouseout="this.style.transform='scale(1)'">
+        <i class="fab fa-whatsapp" style="margin-right: 10px;"></i> Continue to chat
+    </a>
+    <!-- whats app button end -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top" style="margin-bottom: 30px;"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
+<?php 
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = "ganeshjadhav44555@gmail.com"; //add receiver mail 
+    $subject = " Inquiry Regarding Collaboration Opportunities";
+
+    $message ="
+    <html>
+        <head>
+            <title>HTML email</title>
+        </head>
+        <body>
+            <p>Client Details :</p>
+            <table>
+                <tr>
+                    <th> Name </th>
+                    <th> Email </th>
+                    <th> Subject </th>
+                    <th> Message </th>
+                </tr>
+                <tr>
+                    <td>".$name."</td>
+                    <td>".$email."</td>
+                    <td>".$subject."</td>
+                    <td>".$message."</td>
+                </tr>
+            </table>
+        </body>
+    </html>
+    ";
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+    $sent = mail($to,$subject,$message,$headers);
+    if($sent){
+        $_SESSION['message'] ="Thanks for contacting us !";
+    }else{
+        "Error in sending mail.";
+    }
+
+}
+?>
